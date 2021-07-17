bf059e0b084a9d4e482f4a44c9b5a821162c2793
anik1612 <aniksarker1612@gmail.com>
2021-07-17 23:27:13 +0800

    header component ready

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = ({ title }) => {
+	return (
+		<View style={styles.headerStyle}>
+			<Text style={styles.textStyle}>{title}</Text>
+			<StatusBar style='light' />
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	headerStyle: {
+		width: '100%',
+		height: 83,
+		backgroundColor: '#2aafff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	textStyle: {
+		paddingTop: 33,
+		color: 'white',
+		fontWeight: 'bold',
+		fontSize: 24,
+	},
+});
+
+export default Header;
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import Header from '../components/Header';
+
+const HomeScreen = () => {
+	return (
+		<View>
+			<Header title='Guess Game' />
+		</View>
+	);
+};
+
+export default HomeScreen;
